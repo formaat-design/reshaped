2760805f5536267f5ce27d49ad0b8a4bc93f5ef0
Dmitry Belyaev <blv.dmitry@gmail.com>
2021-10-03 18:13:53 +0200

    Webpack starter

---
+++ examples/webpack-starter/webpack.config.js
@@ -0,0 +1,62 @@
+/**
+ * Comment out everything MiniCssExtractPlugin related for a prod css example
+ */
+
+const path = require("path");
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: path.resolve(__dirname, "src/index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name]-[hash].js",
+  },
+  resolve: {
+    modules: ["node_modules"],
+    extensions: [".ts", ".tsx", ".js", ".css"],
+  },
+  performance: {
+    hints: false,
+  },
+  optimization: {
+    splitChunks: {
+      minSize: 10000,
+      maxSize: 250000,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          // {
+          //   loader: MiniCssExtractPlugin.loader,
+          //   options: {
+          //     esModule: false,
+          //   },
+          // },
+          "css-loader",
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.ts(x?)$/,
+        use: ["ts-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Arcade Webpack Example",
+      template: path.resolve(__dirname, "src/index.html"),
+    }),
+    // new MiniCssExtractPlugin()
+  ],
+  devServer: {
+    compress: true,
+    port: 4000,
+  },
+};
