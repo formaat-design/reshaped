ce6e26c5e0db0ad558743885a6f9b9eb06c3bd3e
Dmitry Belyaev <blv.dmitry@gmail.com>
2023-05-06 12:08:13 +0200

    Updated tailwind example

--- examples/integration-tailwind/tailwind.config.cjs
+++ examples/integration-tailwind/tailwind.config.cjs
@@ -1,7 +1,11 @@
 const { getTheme } = require("reshaped/config/tailwind");
 
+const theme = getTheme();
+
+console.log(theme);
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: getTheme(),
+  theme,
 };
