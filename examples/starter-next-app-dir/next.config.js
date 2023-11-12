27b4da58573885b8e99751795baa5785ace56976
Dmitry Belyaev <blv.dmitry@gmail.com>
2023-11-12 10:16:06 +0100

    Updated to Next14

--- examples/starter-next-app-dir/next.config.js
+++ examples/starter-next-app-dir/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
   transpilePackages: ["reshaped"],
+  experimental: {
+    optimizePackageImports: ["reshaped"],
+  },
 };
 
 module.exports = nextConfig;
