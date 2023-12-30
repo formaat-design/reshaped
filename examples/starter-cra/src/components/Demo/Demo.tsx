edb5384d9dbc6f52780c54cf4e440b4ecada086a
Dmitry Belyaev <blv.dmitry@gmail.com>
2023-12-30 19:56:36 +0100

    Update to beta

--- examples/starter-cra/src/components/Demo/Demo.tsx
+++ examples/starter-cra/src/components/Demo/Demo.tsx
@@ -1,17 +1,19 @@
-import { Button, Container, View, Text } from "reshaped";
+import { Container, View, Text, Button } from "reshaped";
 import s from "./Demo.module.css";
 
 const Demo = () => {
   return (
     <View align="center" justify="center" height="100vh">
-      <Container width="800px">
-        <View align="center" gap={3}>
-          <Text variant="display-1">🎉</Text>
-          <Text variant="display-2">Welcome to Reshaped</Text>
-          <Text variant="featured-2" align="center">
+      <Container width="600px">
+        <View gap={3} align="center">
+          <Text variant="title-1">🎉</Text>
+          <Text variant={{ s: "title-6", l: "title-5" }} align="center">
+            Welcome to Reshaped
+          </Text>
+          <Text variant="featured-3" align="center" color="neutral-faded">
             Reshaped is a professionally crafted design system for everyday
             product development made to match your brand. In this example
-            repository we're using it together with Create React App.
+            repository we&apos;re using it together with NextJS
           </Text>
           <View.Item gapBefore={6}>
             <Button
@@ -23,6 +25,7 @@ const Demo = () => {
               Check our website
             </Button>
           </View.Item>
+
           <div className={s.customComponent}>Custom component with styles</div>
         </View>
       </Container>
