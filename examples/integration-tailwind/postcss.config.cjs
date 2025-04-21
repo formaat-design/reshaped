313b3b64b4ca4b896b64cfe2df0349c26e425e34
Dmitry Belyaev <blv.dmitry@gmail.com>
2025-04-21 12:04:24 +0200

    updated tailwind example

--- examples/integration-tailwind/postcss.config.cjs
+++ examples/integration-tailwind/postcss.config.cjs
@@ -3,7 +3,5 @@ const { config } = require("reshaped/config/postcss");
 module.exports = {
   plugins: {
     ...config.plugins,
-    tailwindcss: {},
-    autoprefixer: {},
   },
 };
--- examples/integration-tailwind/src/main.tsx
+++ examples/integration-tailwind/src/main.tsx
@@ -1,10 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const root = createRoot(document.getElementById("root")!);
+root.render(<App />);
